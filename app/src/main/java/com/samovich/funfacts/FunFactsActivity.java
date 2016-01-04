4a702bfb99a68583c3a7c946c47bb1358f85a996
Valery Samovich <valerysamovich@gmail.com>
2016-01-04 11:39:48 -0500

    Initila commit

---
+++ app/src/main/java/com/samovich/funfacts/FunFactsActivity.java
@@ -0,0 +1,13 @@
+package com.samovich.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+    }
+}
