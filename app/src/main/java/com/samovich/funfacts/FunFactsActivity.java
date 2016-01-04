c151ebc5212d17760c2163f580d02892db4410b4
Valery Samovich <valerysamovich@gmail.com>
2016-01-04 17:40:12 -0500

    Add layout

--- app/src/main/java/com/samovich/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/samovich/funfacts/FunFactsActivity.java
@@ -2,8 +2,17 @@ package com.samovich.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+// Layout: http://treehouse-code-samples.s3.amazonaws.com/Android/fun_facts_mockups.png
+// Colors: http://treehouse-code-samples.s3.amazonaws.com/Android/fun_facts_colors.html
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    // View variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
