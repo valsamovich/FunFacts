9ff5002edd7b290c89053deca0e9b4ead06f9867
Valery Samovich <valerysamovich@gmail.com>
2016-01-05 11:45:56 -0500

    Refactor

--- app/src/main/java/com/samovich/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/samovich/funfacts/FunFactsActivity.java
@@ -11,7 +11,11 @@ import java.util.Random;
 // Layout: http://treehouse-code-samples.s3.amazonaws.com/Android/fun_facts_mockups.png
 // Colors: http://treehouse-code-samples.s3.amazonaws.com/Android/fun_facts_colors.html
 
+/**
+ * Created by Valery Samovich on 1/4/16.
+ */
 public class FunFactsActivity extends AppCompatActivity {
+    private FactBook mFactBook = new FactBook();
 
     // Declare view variables
     private TextView mFactTextView;
@@ -29,26 +33,7 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built." };
-
-                // The button was clicked, and TextView update with a new fact
-                String fact = "";
-
-                // Randomly select a fact
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                // fact = randomNumber + "";
-                fact = facts[randomNumber];
+                String fact = mFactBook.getFact();
 
                 // Update the screen with our dynamic fact
                 mFactTextView.setText(fact);
