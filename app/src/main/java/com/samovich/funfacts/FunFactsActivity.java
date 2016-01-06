d3ae896d433049b3e3815fa2e3b6660172457518
Valery Samovich <valerysamovich@gmail.com>
2016-01-06 17:43:08 -0500

    Add sanity and toast

--- app/src/main/java/com/samovich/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/samovich/funfacts/FunFactsActivity.java
@@ -2,10 +2,12 @@ package com.samovich.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FunFactsActivity extends AppCompatActivity {
 
@@ -41,6 +43,14 @@ public class FunFactsActivity extends AppCompatActivity {
                 mShowFactButton.setTextColor(color);
             }
         };
+
         mShowFactButton.setOnClickListener(listener);
+
+        Toast.makeText(
+                FunFactsActivity.this,
+                "Yay! Our Activity was created!",
+                Toast.LENGTH_SHORT).show();
+
+        Log.d("FunFact Activity", "We're logging from the onCreate() method!");
     }
 }
